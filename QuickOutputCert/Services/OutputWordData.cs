03fa81eafdfeb2476da86370705b33530e0b3326
盧彥儒 <a3541336@gmail.com>
2020-06-15 17:50:31 +0800

    6/15更新

--- QuickOutputCert/Services/OutputWordData.cs
+++ QuickOutputCert/Services/OutputWordData.cs
@@ -12,18 +12,19 @@ namespace QuickOutputCert.Services
         public static void OutputReportDetail(List<DataModel> dataModels)
         {
             List<IContentItem> fields = new List<IContentItem>();
-            TableContent tableContent = new TableContent("Row1");
-            var repeatContent = new RepeatContent("NewPage");
+            TableContent tableContent = null;
+            RepeatContent repeatContent = new RepeatContent("NewPage");
             //單號設定
             fields.Add(new FieldContent(("Name"), "123456"));
             int index = 0; //偵測當頁的設備數量，每頁最多10台設備
             dataModels.ForEach((e) =>
             {
-                //if (index == 0)
-                //{
-                //    fields = new List<IContentItem>();
-                //    tableContent = new TableContent("Row1");
-                //}
+                if (index == 0)
+                {
+                    fields = new List<IContentItem>();
+                    tableContent = new TableContent("Row1");
+                }
+
                 tableContent.AddRow(
                     new FieldContent("Index", e.Index),
                     new FieldContent("ProdName", e.ProdName),
@@ -35,28 +36,37 @@ namespace QuickOutputCert.Services
                     new FieldContent("Content1", e.Manufacturer),
                     new FieldContent("Content2", e.DeviceNo)
                     );
-                //if (index == 9)
-                //{
-                //    fields.Add(tableContent);
-                //    repeatContent.AddItem(fields.ToArray());
-                //}
+                if (index == 9)
+                {
+                    repeatContent.AddItem(tableContent);
+                }
                 index = (index + 1) % 10;
             });
+            while (index > 10)
+            {
+                tableContent.AddRow(
+                    new FieldContent("Index", ""),
+                    new FieldContent("ProdName", ""),
+                    new FieldContent("DeviceModel", ""),
+                    new FieldContent("Count", ""),
+                    new FieldContent("Place", ""),
+                    new FieldContent("UseYear", ""),
+                    new FieldContent("Evaluation", ""),
+                    new FieldContent("Content1", ""),
+                    new FieldContent("Content2", "")
+                    );
+                index++;
+            }
             var fillContent = new Content();
             fillContent.Fields.Add(new FieldContent(("Name"), "123456"));
-            fillContent.Tables.Add(tableContent);
-            //fillContent.Repeats.Add(repeatContent);
-
+            fillContent.Repeats.Add(repeatContent);
             File.Copy("Resources/核對單範本.docx", "機電核對單.docx");
-
             using (var outputDocument = new TemplateProcessor("機電核對單.docx").SetRemoveContentControls(true))
             {
-                
+
                 outputDocument.FillContent(fillContent);
                 outputDocument.SaveChanges(); //儲存變更檔案
             }
-
-           // return new FileStream("Copy.docx", FileMode.Open, FileAccess.Read);
         }
     }
 }
